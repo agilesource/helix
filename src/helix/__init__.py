da4e1dd5280a0ee602c4f96a94b596c0ea6ebbb8
Peter Cheng <380329+petercheng@users.noreply.github.com>
2026-04-16 23:32:49 +0800

    chore: bump version to 0.4.23

--- src/helix/__init__.py
+++ src/helix/__init__.py
@@ -4,7 +4,7 @@ Helix - AI Era Software Engineering Methodology New Paradigm
 Core module imports
 """
 
-__version__ = "0.4.22"
+__version__ = "0.4.24"
 __author__ = "Peter"
 __status__ = "Stable"
 
